0fef19f17f9c69805718efcecc9c140c221841bf
Alqama Hasnain <alqama.hasnain@gmail.com>
2024-10-11 12:38:55 +0530

    Navigation, usePathname Hook

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+ 
 import {
   UserGroupIcon,
   HomeIcon,
@@ -13,16 +13,12 @@ import clsx from 'clsx';
 // Depending on the size of the application, this would be stored in a database.
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
-  {
-    name: 'Invoices',
-    href: '/dashboard/invoices',
-    icon: DocumentDuplicateIcon,
-  },
+  { name: 'Invoices', href: '/dashboard/invoices', icon: DocumentDuplicateIcon},
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
-
 export default function NavLinks() {
   const pathname = usePathname();
+ 
   return (
     <>
       {links.map((link) => {
